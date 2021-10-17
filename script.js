f8c85e0b63ae0fda3f47782a15c85755f2f97044
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-16 21:18:36 -0500

    JS Update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// Login button
+console.log("page loaded...");
+// login
 function logOut(element) {
-    element.innerText = 
-    "Logout";
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
 }
 // disappear
 function hide(element) {
